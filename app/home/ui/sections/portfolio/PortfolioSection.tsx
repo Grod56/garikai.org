1c8bb7bdc34c4e5a3ccd8c8e41e1b912ae8bff53
Grod56 <rodneygaryx29@gmail.com>
2025-11-29 10:22:08 +0200

    tweaks

--- app/home/ui/sections/portfolio/PortfolioSection.tsx
+++ app/home/ui/sections/portfolio/PortfolioSection.tsx
@@ -50,7 +50,7 @@ const PortfolioSection = function ({ model }) {
 								model={newReadonlyModel({
 									Component: ImageCardSkeleton,
 									componentModels:
-										Array<ImageCardSkeletonModel>(4).fill(
+										Array<ImageCardSkeletonModel>(5).fill(
 											newReadonlyModel({
 												orientation: "vertical",
 											}),
