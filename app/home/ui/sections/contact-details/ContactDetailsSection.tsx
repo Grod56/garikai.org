f397ac4ad81e002531e3e9224dc69e8710f06ed0
Garikai Gumbo <rodneygaryx29@gmail.com>
2026-04-01 14:02:03 +0200

    updated contact details

--- app/home/ui/sections/contact-details/ContactDetailsSection.tsx
+++ app/home/ui/sections/contact-details/ContactDetailsSection.tsx
@@ -17,16 +17,12 @@ const ContactDetailsSection = function ({ model }) {
 		>
 			<p>
 				For professional inquiries, shoot me an email @{" "}
-				<a href="mailto:providenceuniversalstudios@gmail.com">
-					providenceuniversalstudios@gmail.com
-				</a>
-				. You may also reach out to me on{" "}
-				<a href="https://wa.me/263784310140?text=Hello Garikai, I was referred to this number from your website.">
-					Whatsapp
+				<a href="mailto:business@garikai.org">
+					business@garikai.org
 				</a>
 				. If perhaps something piqued your interest here, or maybe even
 				annoyed you (I take it all in spades I promise 😌), my personal
-				email is right <a href="mailto:rodneygaryx29@gmail.com">here</a>
+				email is right <a href="mailto:me@garikai.org">here</a>
 				. The rest of my socials are down in the footer.
 			</p>
 		</SiteSection>
